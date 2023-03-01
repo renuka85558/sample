c81e1c34e61bc5ba00dfa50154efc39705b97a87
dell <dell@DESKTOP-G03LBA1>
2023-03-01 19:26:08 +0530

    formatted code

--- practice/src/com/renu/CombinationSuminArray.java
+++ practice/src/com/renu/CombinationSuminArray.java
@@ -5,38 +5,38 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CombinationSuminArray {
-	
+
 	public static void main(String[] args) {
-		int[] arr = {1,3,5,6};
-		int sum =6;
-		List<List<Integer>>ll =combinationSum2(arr,sum);
+		int[] arr = { 1, 3, 5, 6 };
+		int sum = 6;
+		List<List<Integer>> ll = combinationSum(arr, sum);
 		System.out.println(ll);
-		
+
 	}
-	public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+
+	public static List<List<Integer>> combinationSum(int[] arr, int target) {
 		List<List<Integer>> ans = new ArrayList<>();
-	    Arrays.sort(candidates);
-	    dfs(0, candidates, target, new ArrayList<>(), ans);
-	    return ans;
-	  }
+		Arrays.sort(arr);
+		dfs(0, arr, target, new ArrayList<>(), ans);
+		return ans;
+	}
 
-	  private static void dfs(int s, int[] candidates, int target, List<Integer> path,
-			  List<List<Integer>> ans) {
-	    if (target < 0) {
-	      return;
-	    }
-	    if (target == 0) {
-	      ans.add(new ArrayList<>(path));
-	      return;
-	    }
+	private static void dfs(int s, int[] arr, int target, List<Integer> path, List<List<Integer>> ans) {
+		if (target < 0) {
+			return;
+		}
+		if (target == 0) {
+			ans.add(new ArrayList<>(path));
+			return;
+		}
 
-	    for (int i = s; i < candidates.length; ++i) {
-	      if (i > s && candidates[i] == candidates[i - 1]) {
-	        continue;
-	      }
-	      path.add(candidates[i]);
-	      dfs(i + 1, candidates, target - candidates[i], path, ans);
-	      path.remove(path.size() - 1);
-	    }
-	  }
-	 }
+		for (int i = s; i < arr.length; ++i) {
+			if (i > s && arr[i] == arr[i - 1]) {
+				continue;
+			}
+			path.add(arr[i]);
+			dfs(i + 1, arr, target - arr[i], path, ans);
+			path.remove(path.size() - 1);
+		}
+	}
+}
